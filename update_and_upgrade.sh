8b0bf486c45996492a79143b4c937a630be1adb8
Nyan Myo Htet <40001598+nyanmyohtet@users.noreply.github.com>
2023-01-20 19:13:32 +0630

    Update update_and_upgrade.sh
    Update update_and_upgrade.sh
    
    Update update_and_upgrade.sh

--- update_and_upgrade.sh
+++ update_and_upgrade.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
 
-echo "Updating package lists..."
+green='\033[0;32m'
+nc='\033[0m' # No Color
+
+# Default value for flag
+do_upgrade=false
+
+while getopts ":u" opt; do
+  case $opt in
+    u)
+      do_upgrade=true
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+echo -e "${green}Updating package lists...${nc}"
 sudo apt update
 
-echo "Upgrading packages..."
-sudo apt upgrade -y
+if [ "$do_upgrade" = true ] ; then
+  echo -e "${green}Upgrading packages...${nc}"
+  sudo apt upgrade -y
+else
+  echo -e "${green}Skipping upgrading packages...${nc}"
+  echo -e "${green}Add '-u' flag to upgrad packages...${nc}"
+fi
 
 # echo "Dist-upgrading packages..."
 # sudo apt dist-upgrade -y
 
-echo "Cleaning up..."
+echo -e "${green}Cleaning up...${nc}"
 sudo apt autoremove -y
 sudo apt autoclean -y
 
-echo "Updating and upgrading process completed!"
+echo -e "${green}Updating and upgrading process completed!${nc}"
