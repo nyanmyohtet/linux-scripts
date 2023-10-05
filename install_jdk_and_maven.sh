dc5cea35b3df94248205f169c255541d3ab429fe
Nyan Myo Htet <40001598+nyanmyohtet@users.noreply.github.com>
2023-10-05 22:01:12 +0630

    Update install_jdk_and_maven.sh

--- install_jdk_and_maven.sh
+++ install_jdk_and_maven.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Update package list
-sudo apt-get update
+sudo apt update
 
 # Install OpenJDK 11
-sudo apt-get install -y openjdk-11-jdk
+sudo apt install -y openjdk-11-jdk
 
 # Install Maven
-sudo apt-get install -y maven
+sudo apt install -y maven
 
 # Verify the installation
 java -version
