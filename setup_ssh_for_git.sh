e20f9dc94bc65fcc1b4c8c64931c4a5dea6fea44
Nyan Myo Htet <40001598+nyanmyohtet@users.noreply.github.com>
2025-11-20 22:23:25 +0800

    setup_ssh_for_git.sh: update to run  without prompting for input (like file location or passphrase)

--- setup_ssh_for_git.sh
+++ setup_ssh_for_git.sh
@@ -15,7 +15,8 @@ git config --global --list
 # read email
 
 # Generate a new SSH key using ed25519 algorithm
-ssh-keygen -t ed25519 -C "$email"
+# run without prompting for input (like file location or passphrase)
+ssh-keygen -t ed25519 -C "$email" -f ~/.ssh/id_ed25519 -N ""
 
 # Add the key to the ssh-agent
 # eval "$(ssh-agent -s)"
